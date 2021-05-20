fafc8769b9ad57925c4a8d196462ffc9a1e009f1
Karolis Balčiūnas <ka.balciunas@ktu.edu>
2021-05-20 12:56:38 +0300

    Remove unneeded includes

--- DogeLang/DogeLang.cpp
+++ DogeLang/DogeLang.cpp
@@ -1,123 +1,13 @@
-#include "llvm/ADT/APFloat.h"
-#include "llvm/ADT/Optional.h"
-#include "llvm/ADT/STLExtras.h"
-#include "llvm/IR/BasicBlock.h"
-#include "llvm/IR/Constants.h"
-#include "llvm/IR/DerivedTypes.h"
-#include "llvm/IR/Function.h"
-#include "llvm/IR/Instructions.h"
-#include "llvm/IR/IRBuilder.h"
-#include "llvm/IR/LLVMContext.h"
 #include "llvm/IR/LegacyPassManager.h"
-#include "llvm/IR/Module.h"
-#include "llvm/IR/Type.h"
-#include "llvm/IR/Verifier.h"
 #include "llvm/Support/FileSystem.h"
 #include "llvm/Support/Host.h"
-#include "llvm/Support/raw_ostream.h"
 #include "llvm/Support/TargetRegistry.h"
 #include "llvm/Support/TargetSelect.h"
 #include "llvm/Target/TargetMachine.h"
-#include "llvm/Target/TargetOptions.h"
-
-#include <algorithm>
-#include <cassert>
-#include <cctype>
-#include <cstdio>
-#include <cstdlib>
-#include <map>
-#include <memory>
-#include <string>
-#include <system_error>
-#include <utility>
-#include <vector>
-
-#include "ExprAST.h"
-#include "BinaryExprAST.h"
-#include "CallExprAST.h"
-#include "ForExprAST.h"
-#include "FunctionAST.h"
-#include "IfExprAST.h"
-#include "NumberExprAST.h"
-#include "PrototypeAST.h"
-#include "UnaryExprAST.h"
-#include "VarExprAST.h"
-#include "VariableExprAST.h"
-#include "Token.h"
-#include "MapContainers.h"
-#include "GlobalPointers.h"
-#include "LogFunctions.h"
-#include "Getters.h"
-#include "Parsers.h"
-#include "BaseTypeGlobals.h"
-#include "Handlers.h"
 #include "MainLogic.h"
 
-using namespace llvm;
-using namespace llvm::sys;
-
-//===----------------------------------------------------------------------===//
-// Top-Level parsing and JIT Driver
-//===----------------------------------------------------------------------===//
-
-//static void InitializeModuleAndPassManager() {
-//    // Open a new module.
-//    TheContext = std::make_unique<LLVMContext>();
-//    TheModule = std::make_unique<Module>("my cool jit", *TheContext);
-//
-//    // Create a new builder for the module.
-//    Builder = std::make_unique<IRBuilder<>>(*TheContext);
-//}
-//
-///// top ::= definition | external | expression | ';'
-//static void MainLoop() {
-//    while (true) {
-//        switch (CurTok) {
-//        case tok_eof:
-//            return;
-//        case ';': // ignore top-level semicolons.
-//            getNextToken();
-//            break;
-//        case tok_def:
-//            HandleDefinition();
-//            break;
-//        case tok_extern:
-//            HandleExtern();
-//            break;
-//        default:
-//            HandleTopLevelExpression();
-//            break;
-//        }
-//    }
-//}
-
-//===----------------------------------------------------------------------===//
-// "Library" functions that can be "extern'd" from user code.
-//===----------------------------------------------------------------------===//
-
-#ifdef _WIN32
-#define DLLEXPORT __declspec(dllexport)
-#else
-#define DLLEXPORT
-#endif
-
-/// putchard - putchar that takes a double and returns 0.
-extern "C" DLLEXPORT double putchard(double X) {
-    fputc((char)X, stderr);
-    return 0;
-}
-
-/// printd - printf that takes a double prints it as "%f\n", returning 0.
-extern "C" DLLEXPORT double printd(double X) {
-    fprintf(stderr, "%f\n", X);
-    return 0;
-}
-
-//===----------------------------------------------------------------------===//
-// Main driver code.
-//===----------------------------------------------------------------------===//
-
-int main() {
+int main()
+{
     // Install standard binary operators.
     // 1 is lowest precedence.
     BinopPrecedence['<'] = 10;
@@ -135,31 +25,32 @@ int main() {
     MainLoop();
 
     // Initialize the target registry etc.
-    InitializeAllTargetInfos();
-    InitializeAllTargets();
-    InitializeAllTargetMCs();
-    InitializeAllAsmParsers();
-    InitializeAllAsmPrinters();
+    llvm::InitializeAllTargetInfos();
+    llvm::InitializeAllTargets();
+    llvm::InitializeAllTargetMCs();
+    llvm::InitializeAllAsmParsers();
+    llvm::InitializeAllAsmPrinters();
 
-    auto TargetTriple = sys::getDefaultTargetTriple();
+    auto TargetTriple = llvm::sys::getDefaultTargetTriple();
     TheModule->setTargetTriple(TargetTriple);
 
     std::string Error;
-    auto Target = TargetRegistry::lookupTarget(TargetTriple, Error);
+    auto Target = llvm::TargetRegistry::lookupTarget(TargetTriple, Error);
 
     // Print an error and exit if we couldn't find the requested target.
     // This generally occurs if we've forgotten to initialise the
     // TargetRegistry or we have a bogus target triple.
-    if (!Target) {
-        errs() << Error;
+    if (!Target)
+    {
+        llvm::errs() << Error;
         return 1;
     }
 
     auto CPU = "generic";
     auto Features = "";
 
-    TargetOptions opt;
-    auto RM = Optional<Reloc::Model>();
+    llvm::TargetOptions opt;
+    auto RM = llvm::Optional<llvm::Reloc::Model>();
     auto TheTargetMachine =
         Target->createTargetMachine(TargetTriple, CPU, Features, opt, RM);
 
@@ -167,25 +58,27 @@ int main() {
 
     auto Filename = "output.o";
     std::error_code EC;
-    raw_fd_ostream dest(Filename, EC, sys::fs::OF_None);
+    llvm::raw_fd_ostream dest(Filename, EC, llvm::sys::fs::OF_None);
 
-    if (EC) {
-        errs() << "Could not open file: " << EC.message();
+    if (EC)
+    {
+        llvm::errs() << "Could not open file: " << EC.message();
         return 1;
     }
 
-    legacy::PassManager pass;
-    auto FileType = CGFT_ObjectFile;
+    llvm::legacy::PassManager pass;
+    auto FileType = llvm::CGFT_ObjectFile;
 
-    if (TheTargetMachine->addPassesToEmitFile(pass, dest, nullptr, FileType)) {
-        errs() << "TheTargetMachine can't emit a file of this type";
+    if (TheTargetMachine->addPassesToEmitFile(pass, dest, nullptr, FileType))
+    {
+        llvm::errs() << "TheTargetMachine can't emit a file of this type";
         return 1;
     }
 
     pass.run(*TheModule);
     dest.flush();
 
-    outs() << "Wrote " << Filename << "\n";
+    llvm::outs() << "Wrote " << Filename << "\n";
 
     return 0;
 }
