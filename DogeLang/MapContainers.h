8be20cce4b2ab6e356c8d58ab0bc53b31049ac41
Karolis Balčiūnas <ka.balciunas@ktu.edu>
2021-05-20 12:07:12 +0300

    Extract parse functions do separate file

--- DogeLang/MapContainers.h
+++ DogeLang/MapContainers.h
@@ -7,4 +7,6 @@
 
 static std::map<std::string, llvm::AllocaInst*> NamedValues;
 static std::map<std::string, std::unique_ptr<PrototypeAST>> FunctionProtos;
+
+/// BinopPrecedence - This holds the precedence for each binary operator that is defined.
 static std::map<char, int> BinopPrecedence;
